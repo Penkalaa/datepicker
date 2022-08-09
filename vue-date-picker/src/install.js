a06cbae16325de5813ca73c4b04267894dd00f7d
Enes Turan <enesturan9215@gmail.com>
2022-08-09 23:08:35 +0300

    bug fix

--- vue-date-picker/src/install.js
+++ vue-date-picker/src/install.js
@@ -1,16 +1,16 @@
 import Datepicker from "./components/DatePicker.vue";
 
-const MultipleDatePicker = {
+const MDatepickerVue = {
  install(Vue) {
   // Let's register our component globally
   // https://vuejs.org/v2/guide/components-registration.html
-  Vue.component("date-picker", Datepicker);
+  Vue.component("m-datepicker-vue", Datepicker);
  }
 };
 
 // Automatic installation if Vue has been added to the global scope.
 if (typeof window !== 'undefined' && window.Vue) {
-    window.Vue.use(MultipleDatePicker);
+    window.Vue.use(MDatepickerVue);
 }
 
-export default MultipleDatePicker;
+export default MDatepickerVue;
